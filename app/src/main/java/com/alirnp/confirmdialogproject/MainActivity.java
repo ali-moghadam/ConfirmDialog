cd39de13d4c3319e3f47e4b4c8a1de92831e3ae6
alipapital <alimoghadam75@gmail.com>
2020-06-24 17:00:24 +0430

    clean

--- app/src/main/java/com/alirnp/confirmdialogproject/MainActivity.java
+++ app/src/main/java/com/alirnp/confirmdialogproject/MainActivity.java
@@ -1,12 +1,6 @@
 package com.alirnp.confirmdialogproject;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.RippleDrawable;
-import android.graphics.drawable.StateListDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,20 +16,10 @@ import com.alirnp.confirmdialog.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
-   public static StateListDrawable makeSelector(int color) {
-      StateListDrawable res = new StateListDrawable();
-      res.setExitFadeDuration(400);
-      res.setAlpha(45);
-      res.addState(new int[]{android.R.attr.state_pressed}, new ColorDrawable(color));
-      res.addState(new int[]{}, new ColorDrawable(Color.TRANSPARENT));
-      return res;
-   }
-
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-
 
       Button button = findViewById(R.id.button);
       button.setOnClickListener(new View.OnClickListener() {
@@ -48,29 +32,21 @@ public class MainActivity extends AppCompatActivity {
 
    private void openDialog() {
       ConfirmDialog dialog = new ConfirmDialog.Builder("Are you sure to delete item ?")
-
-              .setTextPositiveButton("Confirm")
               .setTextNegativeButton("Cancel")
               .setTypeface(Typeface.SANS_SERIF)
-
               .setColorPositiveButton(getResources().getColor(R.color.colorPrimary))
               .setColorNegativeButton(getResources().getColor(R.color.gray))
-
               .setColorTextPositiveButton(getResources().getColor(android.R.color.white))
               .setColorTextNegativeButton(getResources().getColor(android.R.color.tab_indicator_text))
-
               .setColorRipplePositiveButton(getResources().getColor(android.R.color.white))
               .setColorRippleNegativeButton(getResources().getColor(R.color.colorPrimary))
-
               .setIcon(R.drawable.ic_alert)
               .setIconColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
-
               .setMessageBody("For more information about this library, read the document in github.")
-
               .setPositiveButtonListener(new OnClickListener() {
                  @Override
                  public void onClick(DialogFragment dialog, View view) {
-                    Toast.makeText(MainActivity.this, "mButtonPositive", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Positive Button Clicked", Toast.LENGTH_SHORT).show();
                  }
               })
               .setNegativeButtonListener(new OnClickListener() {
